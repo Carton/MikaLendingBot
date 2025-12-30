fb7d69de58a6bb30b8e4f20c35979c4e5673bbde
Carton He <cartonhe@gmail.com>
2025-12-30 23:01:54 +0800

    build(docs): add sphinx dependencies and build tasks
    Add Sphinx and sphinx-rtd-theme as documentation dependencies.
    Include tasks for building HTML docs, PDF, cleaning, and link checking.
    Update docs/conf.py to set language to English.

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 # Poloniexlendingbot documentation build configuration file, created by
 # sphinx-quickstart on Tue Oct 11 16:56:46 2016.
 #
-# This file is execfile()d with the current directory set to its
+# This file is imported with the current directory set to its
 # containing dir.
 #
 # Note that not all possible configuration values are present in this
@@ -68,7 +68,7 @@ release = '1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
